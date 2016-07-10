4f4b761a7e32ab02956165751cf029fd17160952
erica lai <ericalai89@gmail.com>
2016-07-10 22:58:32 +0000

    ordered lessons in our db

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160708193035) do
+ActiveRecord::Schema.define(version: 20160710224608) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -51,8 +51,10 @@ ActiveRecord::Schema.define(version: 20160708193035) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string   "video"
+    t.integer  "row_order"
   end
 
+  add_index "lessons", ["row_order"], name: "index_lessons_on_row_order", using: :btree
   add_index "lessons", ["section_id"], name: "index_lessons_on_section_id", using: :btree
 
   create_table "sections", force: :cascade do |t|
